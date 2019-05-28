b19cab0404b9d0db9cb14a5e0d7a5788ffdf5bc1
keeper <13632373900@163.com>
2019-05-28 10:55:03 +0800

    增加队列长度

--- queue/src/main/java/LinkedBlockingQueue.java
+++ queue/src/main/java/LinkedBlockingQueue.java
@@ -8,8 +8,20 @@ import java.util.concurrent.locks.ReentrantLock;
 public class LinkedBlockingQueue implements BlockingQueue {
     private volatile Node head;
     private volatile Node tail;
+    //队列长度
+    private volatile int length;
+    private int count;
     private ReentrantLock lock = new ReentrantLock();
-    private Condition condition = lock.newCondition();
+    private Condition consumer = lock.newCondition();
+    private Condition provider = lock.newCondition();
+
+    public LinkedBlockingQueue(int count) {
+        this.count = count;
+    }
+
+    public LinkedBlockingQueue() {
+        this.count = 100;
+    }
 
     public void put(Object value) {
         lock.lock();
@@ -17,12 +29,22 @@ public class LinkedBlockingQueue implements BlockingQueue {
             Node h = new Node();
             h.setVal(value);
             if(tail != null){
-                tail.setNext(h);
-                tail = h;
+                //判断是否超过队列长度,阻塞生产者线程
+                if(length >= count){
+                    System.out.println("生成队列已满，线程【"+Thread.currentThread().getName()+"】阻塞等待");
+                    provider.await();
+                }else{
+                    tail.setNext(h);
+                    tail = h;
+                }
             }else{
                 head = tail = h;
+                System.out.println("唤醒所有消费者线程");
+                consumer.signalAll();
             }
-            condition.signalAll();
+            length++;
+        }catch (InterruptedException e){
+            e.printStackTrace();
         }finally {
             lock.unlock();
         }
@@ -33,13 +55,20 @@ public class LinkedBlockingQueue implements BlockingQueue {
         try {
             while(true){
                 if(head == null) {
-                    condition.await();
+                    System.out.println("队列为空，消费者线程【"+Thread.currentThread().getName()+"】阻塞等待");
+                    consumer.await();
                 }else{
                     Node h = head;
+                    //如果只剩下一个节点，即头结点和尾节点相等，同时置为空
                     if (tail == head){
                         tail = head = null;
                     }else{
                         head = h.getNext();
+                    }
+                    if(--length<=0){
+                        //唤醒生产者线程
+                        System.out.println("唤醒所有生产者线程");
+                        provider.signalAll();
                     }
                     return h.getVal();
                 }
--- queue/src/main/java/QueueTest.java
+++ queue/src/main/java/QueueTest.java
@@ -5,15 +5,10 @@ import java.util.concurrent.TimeUnit;
  */
 public class QueueTest {
     public static void main(String[] args) {
-        BlockingQueue queue = new LinkedBlockingQueue();
+        BlockingQueue queue = new LinkedBlockingQueue(10);
         for(int i=0;i<2;i++){
             new Thread(()->{
                 while(true){
-                    try {
-                        TimeUnit.SECONDS.sleep(5);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
                     String val = System.currentTimeMillis() + "";
                     System.out.println(Thread.currentThread().getName() + "生产了" + val);
                     queue.put(val);
@@ -21,9 +16,14 @@ public class QueueTest {
             }).start();
         }
 
-        for(int i=0;i<10;i++){
+        for(int i=0;i<4;i++){
             new Thread(()->{
                 while(true){
+                    try {
+                        TimeUnit.SECONDS.sleep(2);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     System.out.println(Thread.currentThread().getName() + "开始消费----------");
                     Object val = queue.take();
                     System.out.println(Thread.currentThread().getName() + "消费了" + val);
